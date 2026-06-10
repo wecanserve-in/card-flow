4d5bbb6247c98a2f45fdd2ea368db52438dc5e31
Anand Dangi <ananddangii20@gmail.com>
2026-06-10 17:41:01 +0530

    added procfile

--- backend/main.py
+++ backend/main.py
@@ -15,9 +15,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://cardflow.wecanserve.in"
+],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
